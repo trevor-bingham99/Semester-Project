e83ce026eb069149f2fa733357b3018b286f692b
Trevor Bingham <trevor.m.bingham@gmail.com>
2023-11-17 08:22:49 -0700

    add file

---
+++ data_collection.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
