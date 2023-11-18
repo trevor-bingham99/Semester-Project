5ebb610e150bf8ce010997e52168f53c32f0d64c
Trevor Bingham <trevor.m.bingham@gmail.com>
2023-11-17 21:32:37 -0700

    data csv and test code

--- data_collection.py
+++ data_collection.py
@@ -5,3 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+
+
+url = 'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/HousingUnitInventory/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+
+df = pd.read_csv('./Utah_Housing_Unit_Inventory.csv')
