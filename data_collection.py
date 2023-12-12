9dee2026272a1026c5d733f7601ca3ce8f33a47a
Trevor Bingham <trevor.m.bingham@gmail.com>
2023-12-11 21:37:13 -0700

    update data

--- data_collection.py
+++ data_collection.py
@@ -5,9 +5,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+import json
 
 
 url = 'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/HousingUnitInventory/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
 
 df = pd.read_csv('./Utah_Housing_Unit_Inventory.csv')
 
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get(url)
+
+pre = driver.find_element(By.TAG_NAME,"pre").text
+data = json.loads(pre)
+
+df = pd.json_normalize(data['features'])
