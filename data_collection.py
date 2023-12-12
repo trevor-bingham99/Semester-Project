56bf1d9245341772fe21af12d454a38bc722ac01
Trevor Bingham <trevor.m.bingham@gmail.com>
2023-12-11 21:51:58 -0700

    clean df

--- data_collection.py
+++ data_collection.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import json
 
 
-url = 'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/HousingUnitInventory/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+url = 'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/HousingUnitInventory/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json'
 
 df = pd.read_csv('./Utah_Housing_Unit_Inventory.csv')
 
@@ -19,3 +19,7 @@ pre = driver.find_element(By.TAG_NAME,"pre").text
 data = json.loads(pre)
 
 df = pd.json_normalize(data['features'])
+
+new_column_names = {col: col.replace('attributes.', '') for col in df.columns}
+df.rename(columns=new_column_names, inplace=True)
+df.set_index('OBJECTID', inplace=True)
